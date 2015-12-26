5cf64d702d6e6271d6e8ae41cd239f9d396386fc
roman <rwang2006@yahoo.com>
2015-12-26 00:27:11 -0500

    updating account info was fixed and planned out database structure for posts and donations

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   get '/explore' => 'pages#explore'
 
+  get '/user/:id' => 'pages#profile'
+
+  get '/explore' => 'pages#explore'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
